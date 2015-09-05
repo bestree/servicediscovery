c204edf38470ce916b3b6ba3a9d9eaac48bdb83c
bestree007 <shuxingzhang1988@163.com>
2015-09-05 15:34:45 +0800

    register the service instance again when the connection recovered

--- service.watcher/src/main/java/com/ning/service/ZooKeeperServiceRegistryImpl.java
+++ service.watcher/src/main/java/com/ning/service/ZooKeeperServiceRegistryImpl.java
@@ -3,20 +3,22 @@ package com.ning.service;
 import com.ning.service.discovery.NodeChangedListener;
 import com.ning.service.discovery.PathMonitor;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * The service registry impl
  * Created by bestree007 on 2015/9/3.
  */
-public class ZooKeeperServiceRegistryImpl implements ServiceRegistry, NodeChangedListener
+public class ZooKeeperServiceRegistryImpl implements ServiceRegistry, NodeChangedListener, ConnectionStateListener
 {
     Logger logger = LoggerFactory.getLogger(ZooKeeperServiceRegistryImpl.class);
 
@@ -26,18 +28,44 @@ public class ZooKeeperServiceRegistryImpl implements ServiceRegistry, NodeChange
 
     private ConcurrentHashMap<String, PathMonitor> pathMonitorConcurrentHashMap = new ConcurrentHashMap<String, PathMonitor>();
 
+    // this cache used when the connection loss and then reconnected
+    private Set<ServiceInstance> serviceInstancesRegistryCache = Collections.synchronizedSet(new HashSet<ServiceInstance>());
+
     public ZooKeeperServiceRegistryImpl(CuratorFramework curatorClient)
     {
         this.curatorFramework = curatorClient;
+        this.curatorFramework.getConnectionStateListenable().addListener(this);
     }
 
     public boolean registerServiceInstance(ServiceInstance instance)
     {
+        serviceInstancesRegistryCache.add(instance);
         ZooKeeperServiceInstance zkInstance = new ZooKeeperServiceInstance(instance);
         try {
-            curatorFramework.create().creatingParentsIfNeeded().forPath("/" + instance.getName() + "/" + zkInstance.getUniqueID(), Utils.serialize(instance));
+            String path = "/" + instance.getName() + "/" + zkInstance.getUniqueID();
+            // create the ephemeral node
+            Stat stat = curatorFramework.checkExists().forPath(path);
+            if (stat == null) {
+                curatorFramework.create().creatingParentsIfNeeded()
+                        .withMode(CreateMode.EPHEMERAL).forPath(path, Utils.serialize(instance));
+            } else {
+                long ownerID = stat.getEphemeralOwner();
+                long currentSessionID = curatorFramework.getZookeeperClient().getZooKeeper().getSessionId();
+                if (ownerID == currentSessionID) {
+                    curatorFramework.setData().forPath(path, Utils.serialize(instance));
+                } else {
+                    try {
+                        curatorFramework.delete().forPath(path);
+                    } catch (KeeperException.NoNodeException ex) {
+
+                    } finally {
+                        curatorFramework.create().creatingParentsIfNeeded()
+                                .withMode(CreateMode.EPHEMERAL).forPath(path, Utils.serialize(instance));
+                    }
+                }
+            }
         } catch (Exception ex) {
-            logger.error("Exception occurs during the service instance:" + String.valueOf(instance.getName()));
+            logger.error("Exception occurs during the service instance registry:" + String.valueOf(instance.getName()));
             return false;
         }
         return true;
@@ -108,4 +136,18 @@ public class ZooKeeperServiceRegistryImpl implements ServiceRegistry, NodeChange
         this.pathMonitorConcurrentHashMap.clear();
     }
 
+    public void stateChanged(CuratorFramework client, ConnectionState newState)
+    {
+        switch (newState) {
+            case RECONNECTED: {
+                for (ServiceInstance instance : serviceInstancesRegistryCache) {
+                    try {
+                        registerServiceInstance(instance);
+                    } catch (Throwable e) {
+                        logger.error("Error occurs during the recovering.", e);
+                    }
+                }
+            }
+        }
+    }
 }
